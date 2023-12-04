5d16108265e817a033398e5de609cc0d11b6b9e8
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2023-12-04 19:59:56 +0100

    feat: added github pages deploy, minor vite config enhancements

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,34 @@
-import preactPlugin from '@preact/preset-vite'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-    build: {
-        out: 'out',
-        emptyOutDir: true,
-        rollupOptions: {
-            input: ['demo.html'],
+import preactPlugin from '@preact/preset-vite'
+import rollupPluginSizes from 'rollup-plugin-sizes'
+
+const plugins = [preactPlugin()]
+
+export default defineConfig(({ mode, command }) => {
+    console.log(`[Vite] Mode: ${mode}`)
+    console.log(`[Vite] Command: ${command}`)
+
+    return {
+        define: {
+            'process.env.NODE_ENV': JSON.stringify(mode),
         },
-    },
-    server: {
-        port: 3000,
-    },
-    plugins: [preactPlugin()],
+        build: {
+            outDir: 'out/',
+            emptyOutDir: true,
+            minify: mode === 'production' ? 'terser' : false,
+            rollupOptions: {
+                input: ['index.html', 'demo.html'],
+            },
+        },
+        server: {
+            port: 3000,
+        },
+        plugins:
+            command === 'build'
+                ? // mostro alcune metriche sul bundle generato
+                  [...plugins, rollupPluginSizes()]
+                : // altrimenti non usiamo nessun plugin per ora
+                  [...plugins],
+    }
 })
