ecc6cf33c96a4fc3d2cd5e42eeb35b6dfda15eab
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2024-03-05 01:58:40 +0100

    fix: duplicate name in index

--- src/dm-planimetrie/index.js
+++ src/dm-planimetrie/index.js
@@ -1,9 +1,8 @@
 export { Canvas3D } from './Canvas3D.js'
-
 export { Cursor3D } from './Cursor3D.js'
 
 export { PlanimetrieRoom } from './PlanimetrieRoom.js'
 export { PlanimetrieModel } from './PlanimetrieModel.js'
 export { PlanimetrieTool } from './PlanimetrieTool.js'
-export { PlanimetrieRoom, PlanimetrieViewer } from './PlanimetrieViewer.js'
+export { PlanimetrieViewer } from './PlanimetrieViewer.js'
 export { PolylineWidget } from './PolylineWidget.js'
